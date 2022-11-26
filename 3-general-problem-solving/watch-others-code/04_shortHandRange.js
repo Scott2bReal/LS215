502c2ad70b9181c9f05a1b2c66f078521762af8c
Scott Hoecker <hoeckers@gmail.com>
2022-11-26 14:45:11 -0600

    Getting close on ranges

--- 3-general-problem-solving/watch-others-code/04_shortHandRange.js
+++ 3-general-problem-solving/watch-others-code/04_shortHandRange.js
@@ -35,40 +35,82 @@ Algorithm:
   - Define individual separator and range separators
   - Init final array
   - Split input by individual sep (',')
-  - For each range in resulting array of ranges (with index)
-    - Split range by range separator
-    - For each element of that range array
-      loop
-      - If ranges index is 0, initialize counter to first element of that range array as a number
-      - Add counter (as string) to final array
-      - increment counter by 1
-      - Else
-        - Check if counter as string meets regex for next item in range array
-        - If it does, add counter as string to final array and break
-        - If not, add counter as string to final array and continue
 
-  - Return final array joined
+  - Map resulting array into expanded ranges (nested array)
+    - if element includes range sep, expand range
+    - if not, just return char
+
+  - Init counter (helper function)
+
+  - For each element in resulting array
+    - If is an array, push expanded range to finalArray
+    - If not, determine value and push to finalArray (helper?)
+
+  This works for everything but the uncommented test case. Need to figure out
+  how to determine range boundaries for ranges with more than two entries...
 */
 
 RANGES_SEPARATOR = ', '
 RANGE_SEPARATOR = /[\:\-..\s]/g
 
+function rangeBoundaries(rangeString) {
+  const range = rangeString.split(RANGE_SEPARATOR)
+  return [range[0], range[1]]
+}
+
+function initCounter(arg) {
+  return Array.isArray(arg) ? Number(arg[0]) : Number(arg)
+}
+
+function determineValue(element, counter) {
+  while (true) {
+    if (String(counter).endsWith(element)) {
+      return String(counter);
+    }
+    counter += 1;
+  }
+}
+
+function expandRange(rangeBoundaries, counter) {
+  const start = determineValue(rangeBoundaries[0], counter);
+  const end = determineValue(rangeBoundaries[1], Number(start))
+  counter = Number(start);
+  const nums = [];
+
+  while (counter <= end) {
+    nums.push(String(counter));
+    counter += 1;
+  }
+
+  return nums.join(', ')
+}
+
 function rangeInterpreter(originalString) {
-  const finalArray = [];
-  const ranges = originalString.split(RANGES_SEPARATOR);
-  console.log('ranges: ', ranges)
+  const nums = []
 
-  ranges.forEach((range, rangeIdx) => {
-    const boundaries = range.split(RANGE_SEPARATOR);
-
-
+  const ranges = originalString.split(RANGES_SEPARATOR).map((element) => {
+    return element.match(RANGE_SEPARATOR) ? rangeBoundaries(element) : element
   })
+
+  let counter = initCounter(ranges[0])
+
+  ranges.forEach((element) => {
+    if (Array.isArray(element)) {
+      nums.push(expandRange(element, counter));
+      counter = Number(element[1]);
+    } else {
+      nums.push(determineValue(element, counter));
+      counter = Number(determineValue(element, counter));
+    }
+  })
+
+  return nums.join(', ');
 }
 
 // Examples
-console.log(rangeInterpreter("1, 3, 7, 2, 4, 1" )); // 1, 3, 7, 12, 14, 21
-console.log(rangeInterpreter("1-3, 1-2")); // 1, 2, 3, 11, 12
-console.log(rangeInterpreter("1:5:2")); // 1, 2, 3, 4, 5, 6, ... 12
-console.log(rangeInterpreter("104-2")); // 104, 105, ...112
-console.log(rangeInterpreter("104-02")); // 103, 105, ... 202
-console.log(rangeInterpreter("545, 64:11")); // 545, 564, 565, ... 611
+// console.log(rangeInterpreter('1, 3, 7, 2, 4, 1')) // 1, 3, 7, 12, 14, 21
+// console.log(rangeInterpreter('1-3, 1-2')) // 1, 2, 3, 11, 12
+console.log(rangeInterpreter('1:5:2')) // 1, 2, 3, 4, 5, 6, ... 12
+// console.log(rangeInterpreter('104-2')) // 104, 105, ...112
+// console.log(rangeInterpreter('104-02')) // 103, 105, ... 202
+// console.log(rangeInterpreter('545, 64:11')) // 545, 564, 565, ... 611
